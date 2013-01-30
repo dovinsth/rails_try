8541976e590b78e9c6871a947e0e368674037414
dovinsth <laxman.shrestha@cloudfactory.com>
2013-01-30 14:58:27 +0545

    adding all the application files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytestsite::Application.config.secret_token = '84ecdea1125ea3b388681d2120baf056299ef1c7f580d6826b8694e4d58399d4cad87a3765d3590c0bd34502246d419a6360509cdf4ed34069b91b80ebddebf2'
